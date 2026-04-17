340527f7892d8a009c36f1e0a0267b90484a5f15
nitisnkn <165454263+nitisnkn@users.noreply.github.com>
2026-04-17 18:20:46 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; 
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'screens/auth/home_screen.dart';
-import 'screens/auth/login_screen.dart';
 
-import 'screens/auth/loading_screen.dart'; 
+// 📌 1. เพิ่มการ Import หน้า ProfileScreen (ตรวจสอบ Path ให้ถูกต้อง)
+import 'screens/auth/profile_screen.dart'; 
 
 void main() async { 
   WidgetsFlutterBinding.ensureInitialized(); 
@@ -29,29 +27,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      // 📌 3. ใช้ StreamBuilder ครอบไว้เพื่อเช็คว่าล็อกอินอยู่หรือไม่
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          // 1. ระหว่างรอ Firebase เช็คข้อมูล (ปกติจะเร็วมาก) 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              backgroundColor: Colors.white,
-              body: Center(child: CircularProgressIndicator(color: Colors.green)),
-            );
-          }
-
-          //ถ้า Firebase พบว่า "เคยล็อกอินไว้แล้ว"
-          // ให้เรียกหน้า LoadingScreen แล้วแอบส่ง HomeScreen() ไปรอไว้
-          if (snapshot.hasData) {
-            return const LoadingScreen(nextPage: HomeScreen()); 
-          }
-
-          //ถ้า "ยังไม่ได้ล็อกอิน" หรือเพิ่งโหลดแอปครั้งแรก
-          // ให้เรียกหน้า LoadingScreen แล้วส่ง LoginScreen() ไปรอไว้
-          return const LoadingScreen(nextPage: LoginScreen());
-        },
-      ),
+      // 📌 2. เปลี่ยนตรงนี้จาก HomeScreen() ให้เป็น ProfileScreen()
+      home: const ProfileScreen(), 
     );
   }
 }
